d7545094ff1c27e810904b8e53a0ef4c7da62c51
ShauryaMadhav <87056842+ShauryaMadhav@users.noreply.github.com>
2022-02-21 13:52:31 +0530

    initial commit

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Add: 7a - 3b + 5c + 2a - 3b - 4c + c - 4a + b + 5a + 7a + 2b + 8b - a + 1",
+    answer: "16a + 5b + 2c + 1",
+    options: [
+      "16a - 5b + 3c + 1",
+      "16a + 5b - 3c + 2",
+      "16a + 5b + 2c + 1",
+      "16a - 6b - 2c - 1"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Express 3249/3481 in power notation",
+    answer: "57/59",
+    options: [
+      "57/58",
+      "47/49",
+      "37/39",
+      "57/59"
+    ]
+  },
+    {
+    numb: 3,
+    question: "a²(b² - c²) + b²(c² - a²) + c²(a² - b²)",
+    answer: "0",
+    options: [
+      "2",
+      "0",
+      "a²b² - b²c² + a²c²",
+      "1"
+    ]
+  },
+    {
+    numb: 4,
+    question: "[{(-2/3)⁻³}⁻⁴]⁻¹/¹²",
+    answer: "-2/3",
+    options: [
+      "0",
+      "1",
+      "-2/3",
+      "3/2²⁴"
+    ]
+  },
+    {
+    numb: 5,
+    question: "3²ˣ⁺¹ ÷ 9 = 27.Find the value of 5²ˣ⁺¹",
+    answer: "3125",
+    options: [
+      "2125",
+      "3025",
+      "3125",
+      "4025"
+    ]
+  },
+];
